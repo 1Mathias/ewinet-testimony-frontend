40813a64501c5bdf1498a39baa42d4eb71463b8c
1Mathias <mathiasabdisa@gmail.com>
2022-11-17 06:16:03 +0300

    ewinetegna updates

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
     useEffect(() => {
         const fetchPosts = async () => {
             const res = await axios.get("/posts" + search);
-            console.log(res.data)
+
             setPosts(res.data);
         }
         fetchPosts();
--- src/pages/videos/videos.jsx
+++ src/pages/videos/videos.jsx
@@ -7,60 +7,60 @@ import Video from '../../components/video/video';
 import './videos.css';
 
 const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
-  const PF = "http://localhost:5000/images/";
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+const PF = "http://localhost:5000/images/";
 
 export default function videos() {
   return (
     <>
-    <div className='header'>
-      <div>
+      <div className='header'>
+        <div>
           <img className="headerImg" src="https://cdn.pixabay.com/photo/2017/03/29/15/18/tianjin-2185510_960_720.jpg" alt="logo" />
         </div>
-    </div>
-    <h1 className="text-center m-5 text-secondary">Testemonial Videos</h1>
-    <Container>
-      <h4 className="text-secondary">Orthodox</h4>
-      <Carousel responsive={responsive} centerMode={true} swipeable={true} draggable={true} infinite={true} keyBoardControl={true} renderDotsOutside={true}>
-        <Video src="https://www.youtube.com/embed/w-KF26jebQI" url={PF+`ortho1.png`} />
-        <Video src="https://www.youtube.com/embed/M6eKSuFYWF4" url={PF+`ortho2.png`}  />
-        <Video src="https://www.youtube.com/embed/VmN1p2HZBK8" url={PF+`ortho3.png`}  />
-        <Video src="https://www.youtube.com/embed/BBGfku6G5Fw" url={PF+`ortho4.png`}  />
-      </Carousel>
-      <hr />
-      <h4 className="text-secondary">Islam</h4>
-      <Carousel responsive={responsive} centerMode={true} swipeable={true} draggable={true} infinite={true} keyBoardControl={true} renderDotsOutside={true}>
-        <Video src="https://www.youtube.com/embed/O5I9l2ZHFvU" url={PF+`islam1.png`}  />
-        <Video src="https://www.youtube.com/embed/IIReNZuPM6M" url={PF+`islam2.png`}  />
-        <Video src="https://www.youtube.com/embed/GDn2SSpEL-Y" url={PF+`islam3.png`}  />
-        <Video src="https://www.youtube.com/embed/uj8pK7VfBto" url={PF+`islam4.png`}  />
-      </Carousel>
-      <hr />
-      <h4 className="text-secondary">Adventist</h4>
-      <Carousel responsive={responsive} centerMode={true} swipeable={true} draggable={true} infinite={true} keyBoardControl={true} renderDotsOutside={true}>
-        <Video src="https://www.youtube.com/embed/3N6PkhmIQ-o" url={PF+`ad1.png`}  />
-        <Video src="https://www.youtube.com/embed/Y-2LaCPST9o" url={PF+`ortho1.png`}  />
-        <Video src="https://www.youtube.com/embed/Y4pto3M2750" url={PF+`islam1.png`}  />
-        <Video src="https://www.youtube.com/embed/Y4pto3M2750" url={PF+`ortho2.png`}  />
-      </Carousel>
-    </Container>
+      </div>
+      <h1 className="text-center m-5 text-secondary">Testimonial Videos</h1>
+      <Container>
+        <h4 className="text-secondary">Orthodox</h4>
+        <Carousel responsive={responsive} centerMode={true} swipeable={true} draggable={true} infinite={true} keyBoardControl={true} renderDotsOutside={true}>
+          <Video src="https://www.youtube.com/embed/w-KF26jebQI" url={PF + `ortho1.png`} />
+          <Video src="https://www.youtube.com/embed/M6eKSuFYWF4" url={PF + `ortho2.png`} />
+          <Video src="https://www.youtube.com/embed/VmN1p2HZBK8" url={PF + `ortho3.png`} />
+          <Video src="https://www.youtube.com/embed/BBGfku6G5Fw" url={PF + `ortho4.png`} />
+        </Carousel>
+        <hr />
+        <h4 className="text-secondary">Islam</h4>
+        <Carousel responsive={responsive} centerMode={true} swipeable={true} draggable={true} infinite={true} keyBoardControl={true} renderDotsOutside={true}>
+          <Video src="https://www.youtube.com/embed/O5I9l2ZHFvU" url={PF + `islam1.png`} />
+          <Video src="https://www.youtube.com/embed/IIReNZuPM6M" url={PF + `islam2.png`} />
+          <Video src="https://www.youtube.com/embed/GDn2SSpEL-Y" url={PF + `islam3.png`} />
+          <Video src="https://www.youtube.com/embed/uj8pK7VfBto" url={PF + `islam4.png`} />
+        </Carousel>
+        <hr />
+        <h4 className="text-secondary">Adventist</h4>
+        <Carousel responsive={responsive} centerMode={true} swipeable={true} draggable={true} infinite={true} keyBoardControl={true} renderDotsOutside={true}>
+          <Video src="https://www.youtube.com/embed/3N6PkhmIQ-o" url={PF + `ad1.png`} />
+          <Video src="https://www.youtube.com/embed/Y-2LaCPST9o" url={PF + `ortho1.png`} />
+          <Video src="https://www.youtube.com/embed/Y4pto3M2750" url={PF + `islam1.png`} />
+          <Video src="https://www.youtube.com/embed/Y4pto3M2750" url={PF + `ortho2.png`} />
+        </Carousel>
+      </Container>
     </>
   )
 }
